0b385442a2e48319eccbeef2b33d1d90d9213796
DASOM <n980805@gmail.com>
2024-03-07 16:01:44 +0900

    refactor: Id 변경 (id -> name)

--- src/main/java/com/group/attendancemanagementsystem/repository/team/TeamRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/team/TeamRepository.java
@@ -2,7 +2,7 @@ package com.group.attendancemanagementsystem.repository.team;
 
 import com.group.attendancemanagementsystem.domain.team.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, String> {
+
 }
