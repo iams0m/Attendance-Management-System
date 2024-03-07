8e31d3e8747521172cccb7f209ccff54b40b0cd0
DASOM <n980805@gmail.com>
2024-03-07 12:48:38 +0900

    build: initial setting

---
+++ src/test/java/com/group/attendancemanagementsystem/AttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.attendancemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
