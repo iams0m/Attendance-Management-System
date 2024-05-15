3d8ac2f5a9ef66eb4dd3787ed30a88e7dc711959
iams0m <n980805@gmail.com>
2024-05-15 19:29:31 +0900

    Refacor: 매니저 관련 로직 수정

--- src/main/java/com/group/attendancemanagementsystem/service/employee/EmployeeService.java
+++ src/main/java/com/group/attendancemanagementsystem/service/employee/EmployeeService.java
@@ -8,6 +8,7 @@ import com.group.attendancemanagementsystem.dto.employee.response.EmployeeFindAl
 import com.group.attendancemanagementsystem.repository.employee.EmployeeRepository;
 import com.group.attendancemanagementsystem.repository.team.TeamRepository;
 import jakarta.transaction.Transactional;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +32,12 @@ public class EmployeeService {
 
         if (request.getTeamName() != null) {
             team = teamRepository.findByName(request.getTeamName())
-                    .orElseThrow(IllegalArgumentException::new);
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 팀입니다."));
+        }
+
+        // 만약 직원의 역할이 MANAGER이고, 팀에 이미 MANAGER가 존재 한다면 예외 발생
+        if (request.getRole() == Role.MANAGER && team != null && team.hasManager()) {
+            throw new IllegalArgumentException("이미 팀에 매니저가 존재합니다.");
         }
 
         Employee employee = new Employee(
@@ -44,8 +50,15 @@ public class EmployeeService {
 
         employeeRepository.save(employee);
 
-        if (employee.getRole() == Role.MANAGER) {
-            team.setManager(request.getName());
+        // 만약 회원의 역할이 MANAGER 라면, 팀의 manager 필드 업데이트
+        if (employee.getRole() == Role.MANAGER && team != null) {
+            team.setManagerName(employee.getName());
+            teamRepository.save(team);
+        }
+
+        // 팀에 직원 추가
+        if (team != null) {
+            team.addEmployee(employee);
             teamRepository.save(team);
         }
     }
