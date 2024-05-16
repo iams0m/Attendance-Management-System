6911d40ce514d13dee9e2a78cc18af5da7f37fb5
iams0m <n980805@gmail.com>
2024-05-17 02:09:08 +0900

    Feat: 출퇴근 리포지토리 생성

---
+++ src/main/java/com/group/attendancemanagementsystem/repository/commute/CommuteRepository.java
@@ -0,0 +1,11 @@
+package com.group.attendancemanagementsystem.repository.commute;
+
+import com.group.attendancemanagementsystem.domain.commute.Commute;
+import com.group.attendancemanagementsystem.domain.employee.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface CommuteRepository extends JpaRepository<Commute, Long> {
+    boolean existsCommuteByEmployeeAndDate(Employee employee, LocalDate date);
+}
