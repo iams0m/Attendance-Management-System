c5dcbe929e38e8f4fa15ab369f862cbef292dc77
iams0m <n980805@gmail.com>
2024-05-17 02:10:27 +0900

    Feat: 출퇴근 컨트롤러 생성 후 출근 메서드 추가

---
+++ src/main/java/com/group/attendancemanagementsystem/controller/commute/CommuteController.java
@@ -0,0 +1,20 @@
+package com.group.attendancemanagementsystem.controller.commute;
+
+import com.group.attendancemanagementsystem.service.commute.CommuteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommuteController {
+    private final CommuteService commuteService;
+
+    public CommuteController(CommuteService commuteService) {
+        this.commuteService = commuteService;
+    }
+
+    @PostMapping("/commute/start")
+    public void startedCommute(@RequestParam(name = "employeeId") Long employeeId) {
+        commuteService.startedCommute(employeeId);
+    }
+}
