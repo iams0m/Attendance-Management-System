4ec071282dab150cc448ff988cde30b1400c1164
iams0m <n980805@gmail.com>
2024-05-19 22:34:05 +0900

    Feat: 근무시간 조회 Request DTO 생성

--- src/main/java/com/group/attendancemanagementsystem/dto/commute/request/CommuteByAllDayOfMonthRequest.java
+++ src/main/java/com/group/attendancemanagementsystem/dto/commute/request/CommuteByAllDayOfMonthRequest.java
@@ -1,2 +1,27 @@
-package com.group.attendancemanagementsystem.dto.commute.request;public class CommuteByAllDayOfMonthRequest {
+package com.group.attendancemanagementsystem.dto.commute.request;
+
+public class CommuteByAllDayOfMonthRequest {
+
+    private Long id;
+    private String date;
+
+    protected CommuteByAllDayOfMonthRequest() {
+    }
+
+    public CommuteByAllDayOfMonthRequest(Long id) {
+        this.id = id;
+    }
+
+    public CommuteByAllDayOfMonthRequest(Long id, String date) {
+        this.id = id;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
