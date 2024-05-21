6cf552a431bc37751d8c558edfd943798ac22a89
iams0m <n980805@gmail.com>
2024-05-21 12:04:45 +0900

    Feat: 근무시간 조회 기능 작성

--- src/main/java/com/group/attendancemanagementsystem/repository/commute/CommuteRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/commute/CommuteRepository.java
@@ -5,9 +5,12 @@ import com.group.attendancemanagementsystem.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface CommuteRepository extends JpaRepository<Commute, Long> {
     boolean existsCommuteByEmployeeAndDate(Employee employee, LocalDate date);
     Optional<Commute> findCommuteByEmployeeAndDate(Employee employee, LocalDate date);
+
+    List<Commute> findCommutesByEmployeeIdAndDateBetween(Long id, LocalDate start, LocalDate end);
 }
