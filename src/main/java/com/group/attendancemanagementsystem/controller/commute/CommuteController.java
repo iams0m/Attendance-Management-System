70bb152c7ccd43877d595ebc5fe5da9e376d5290
iams0m <n980805@gmail.com>
2024-05-21 12:01:47 +0900

    Feat: 근무시간 조회 메서드 추가

--- src/main/java/com/group/attendancemanagementsystem/controller/commute/CommuteController.java
+++ src/main/java/com/group/attendancemanagementsystem/controller/commute/CommuteController.java
@@ -1,6 +1,9 @@
 package com.group.attendancemanagementsystem.controller.commute;
 
+import com.group.attendancemanagementsystem.dto.commute.request.CommuteByAllDayOfMonthRequest;
+import com.group.attendancemanagementsystem.dto.commute.response.CommuteResponse;
 import com.group.attendancemanagementsystem.service.commute.CommuteService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,11 @@ public class CommuteController {
     @PostMapping("/commute/end")
     public void endedCommute(@RequestParam(name = "employeeId") Long employeeId) {
         commuteService.endedCommute(employeeId);
+    }
+
+    @GetMapping("/commute")
+    public CommuteResponse employeeCommuteByAllDayOfMonth(@RequestParam(name = "id") Long id, @RequestParam(name = "date") String date) {
+        CommuteByAllDayOfMonthRequest request = new CommuteByAllDayOfMonthRequest(id, date);
+        return commuteService.employeeCommuteByAllDayOfMonth(request);
     }
 }
