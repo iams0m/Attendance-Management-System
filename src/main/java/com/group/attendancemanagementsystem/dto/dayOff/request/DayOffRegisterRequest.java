68876cf9c3e3d41cacffc62719f4a5590a41ae7c
iams0m <n980805@gmail.com>
2024-05-23 19:49:09 +0900

    Feat: 연차 관련 Request DTO 생성

---
+++ src/main/java/com/group/attendancemanagementsystem/dto/dayOff/request/DayOffRegisterRequest.java
@@ -0,0 +1,26 @@
+package com.group.attendancemanagementsystem.dto.dayOff.request;
+
+import java.time.LocalDate;
+
+public class DayOffRegisterRequest {
+
+    private Long id;
+
+    private LocalDate date;
+
+    protected DayOffRegisterRequest() {
+    }
+
+    public DayOffRegisterRequest(Long id, LocalDate date) {
+        this.id = id;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
