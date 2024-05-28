76ffc962b1192acb80e1aeb0037e117790583a7f
iams0m <n980805@gmail.com>
2024-05-28 16:39:51 +0900

    Feat: 연차 사용 여부 추가

--- src/main/java/com/group/attendancemanagementsystem/dto/commute/response/DetailResponse.java
+++ src/main/java/com/group/attendancemanagementsystem/dto/commute/response/DetailResponse.java
@@ -6,13 +6,15 @@ public class DetailResponse {
 
     private LocalDate date;
     private Long workingMinutes;
+    private boolean usingDayOff;
 
     protected DetailResponse() {
     }
 
-    public DetailResponse(LocalDate date, Long workingMinutes) {
+    public DetailResponse(LocalDate date, Long workingMinutes, boolean usingDayOff) {
         this.date = date;
         this.workingMinutes = workingMinutes;
+        this.usingDayOff = usingDayOff;
     }
 
     public LocalDate getDate() {
@@ -21,5 +23,9 @@ public class DetailResponse {
 
     public Long getWorkingMinutes() {
         return workingMinutes;
+    }
+
+    public boolean isUsingDayOff() {
+        return usingDayOff;
     }
 }
