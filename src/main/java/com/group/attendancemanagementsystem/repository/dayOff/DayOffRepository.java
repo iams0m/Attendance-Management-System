0c369d39332cfd350f6584e34a7f10cac8e22351
iams0m <n980805@gmail.com>
2024-05-28 17:02:35 +0900

    Feat: 휴가 사용 여부 확인 기능 작성

--- src/main/java/com/group/attendancemanagementsystem/repository/dayOff/DayOffRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/dayOff/DayOffRepository.java
@@ -5,8 +5,10 @@ import com.group.attendancemanagementsystem.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface DayOffRepository extends JpaRepository<DayOff, Long> {
     long countDayOffByEmployee(Employee employee);
     boolean existsDayOffByEmployeeAndDate(Employee employee, LocalDate date);
+    List<DayOff> findDaysOffByEmployeeIdAndDateBetween(Long employeeId, LocalDate startDate, LocalDate endDate);
 }
