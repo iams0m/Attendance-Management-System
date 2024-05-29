4a5bbd8fa2f3f680e5b53d4ae5a1f401a1387817
iams0m <n980805@gmail.com>
2024-05-29 19:47:22 +0900

    Feat: 초과 근무 계산 메서드 추가

--- src/main/java/com/group/attendancemanagementsystem/controller/commute/CommuteController.java
+++ src/main/java/com/group/attendancemanagementsystem/controller/commute/CommuteController.java
@@ -2,11 +2,14 @@ package com.group.attendancemanagementsystem.controller.commute;
 
 import com.group.attendancemanagementsystem.dto.commute.request.CommuteByAllDayOfMonthRequest;
 import com.group.attendancemanagementsystem.dto.commute.response.CommuteResponse;
+import com.group.attendancemanagementsystem.dto.commute.response.OvertimeResponse;
 import com.group.attendancemanagementsystem.service.commute.CommuteService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommuteController {
@@ -30,5 +33,10 @@ public class CommuteController {
     public CommuteResponse employeeCommuteByAllDayOfMonth(@RequestParam(name = "id") Long id, @RequestParam(name = "date") String date) {
         CommuteByAllDayOfMonthRequest request = new CommuteByAllDayOfMonthRequest(id, date);
         return commuteService.getEmployeeMonthlyWorkHours(request);
+    }
+
+    @GetMapping("/overtime")
+    public List<OvertimeResponse> employeeOvertimeList(@RequestParam(name = "date") String date) {
+        return commuteService.employeeOvertimeList(date);
     }
 }
