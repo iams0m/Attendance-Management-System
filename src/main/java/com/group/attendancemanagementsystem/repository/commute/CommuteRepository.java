2483259192b80a03be1516c3e16d6e856247186f
iams0m <n980805@gmail.com>
2024-05-31 19:45:00 +0900

    Feat: @Query를 이용한 employee_id 조회

--- src/main/java/com/group/attendancemanagementsystem/repository/commute/CommuteRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/commute/CommuteRepository.java
@@ -3,6 +3,7 @@ package com.group.attendancemanagementsystem.repository.commute;
 import com.group.attendancemanagementsystem.domain.commute.Commute;
 import com.group.attendancemanagementsystem.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,4 +14,7 @@ public interface CommuteRepository extends JpaRepository<Commute, Long> {
     Optional<Commute> findCommuteByEmployeeAndDate(Employee employee, LocalDate date);
 
     List<Commute> findCommutesByEmployeeIdAndDateBetween(Long id, LocalDate start, LocalDate end);
+
+    @Query("SELECT DISTINCT a.employee.id FROM Commute a")
+    List<Long> findByEmployeeId();
 }
