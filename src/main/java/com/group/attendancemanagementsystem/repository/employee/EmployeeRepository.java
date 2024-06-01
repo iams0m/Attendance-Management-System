9d82ea7b89515cab7c69e9f6628adac708d15745
iams0m <n980805@gmail.com>
2024-06-01 16:53:35 +0900

    Refactor: @Param 어노테이션 추가

--- src/main/java/com/group/attendancemanagementsystem/repository/employee/EmployeeRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/employee/EmployeeRepository.java
@@ -2,11 +2,11 @@ package com.group.attendancemanagementsystem.repository.employee;
 
 import com.group.attendancemanagementsystem.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findEmployeeById(Long employeeId);
-
+    Optional<Employee> findEmployeeById(@Param("employeeId") Long employeeId);
     String findNameById(Long id);
 }
