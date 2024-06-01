601c5d587cfbfc556ae54da521ac5e9e67154cd9
iams0m <n980805@gmail.com>
2024-06-01 16:54:46 +0900

    Feat: 직원 이름 조회 @Query 어노테이션 추가

--- src/main/java/com/group/attendancemanagementsystem/repository/employee/EmployeeRepository.java
+++ src/main/java/com/group/attendancemanagementsystem/repository/employee/EmployeeRepository.java
@@ -2,11 +2,14 @@ package com.group.attendancemanagementsystem.repository.employee;
 
 import com.group.attendancemanagementsystem.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findEmployeeById(@Param("employeeId") Long employeeId);
+
+    @Query("SELECT e.name FROM Employee e WHERE e.id = :id")
     String findNameById(Long id);
 }
