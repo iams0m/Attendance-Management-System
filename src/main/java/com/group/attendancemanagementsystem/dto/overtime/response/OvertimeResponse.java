ff93e7787fbb1cb4b1602333bed2e4f4feac33ba
iams0m <n980805@gmail.com>
2024-06-13 10:22:36 +0900

    Refactor: OvertimeResponse 타입 변경 (class -> record)

--- src/main/java/com/group/attendancemanagementsystem/dto/overtime/response/OvertimeResponse.java
+++ src/main/java/com/group/attendancemanagementsystem/dto/overtime/response/OvertimeResponse.java
@@ -1,29 +1,4 @@
 package com.group.attendancemanagementsystem.dto.overtime.response;
 
-public class OvertimeResponse {
-
-    private Long id;
-    private String name;
-    private int overtimeMinutes;
-
-    protected OvertimeResponse() {
-    }
-
-    public OvertimeResponse(Long id, String name, int overtimeMinutes) {
-        this.id = id;
-        this.name = name;
-        this.overtimeMinutes = overtimeMinutes;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getOvertimeMinutes() {
-        return overtimeMinutes;
-    }
+public record OvertimeResponse(Long id, String name, int overtimeMinutes) {
 }
